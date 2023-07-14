0a7cf5df1ee58d9e54aca23a3e5edcfafb14b3fd
İsmail <olgunerismail2@gmail.com>
2023-07-14 10:29:22 +0300

    2 tane IconButton eklenmesi

--- material-components-flutter-codelabs-101-starter/mdc_100_series/lib/home.dart
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/home.dart
@@ -23,8 +23,42 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
-      // TODO: Add app bar (102)
+    return Scaffold(
+      appBar: AppBar(
+        // TODO: Add trailing buttons (102)
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        title: const Text('SHRINE'),
+        // TODO: Add trailing buttons (102)
+      ),
+
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
